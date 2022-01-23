e313b03d489a987fca060a8fb6403659830de7ca
Janne Jaanila <janne.jaanila@gmail.com>
2022-01-23 13:33:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    Hello
+  </main>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+main {
+  background-color: red;
+}
+</style>
