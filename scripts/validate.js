3cb38c4c0b561faaca0954d443a4201a377fe9ec
Janne Jaanila <janne.jaanila@gmail.com>
2022-02-02 22:34:35 +0200

    Install husky hooks

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,5 +2,5 @@ const campaignJson = require('../campaign')
 const validate = require('jsonschema').validate
 const CampaignSchema = require('../src/schemas/campaign.json')
 
-console.log(validate(campaignJson, CampaignSchema, { throwAll: true }))
+validate(campaignJson, CampaignSchema, { throwAll: true })
 console.log('\ncampaign.json is valid!')
