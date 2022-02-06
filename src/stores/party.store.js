a6ec0e3130b78cdd4b2d856a71c395c4dd3f59db
Janne Jaanila <janne.jaanila@gmail.com>
2022-02-06 22:25:19 +0200

    Refactor

--- src/stores/party.store.js
+++ src/stores/party.store.js
@@ -2,6 +2,24 @@ import { encounterDifficulties } from '../tables'
 
 export const LOCAL_STORAGE_STATE_KEY = 'campaigner-party'
 
+const updateEncounterLimits = state => {
+  state.encounterLimits = state.characters.reduce(
+    (limits, character) => {
+      const characterLimits = encounterDifficulties.find(ed => ed.characterLevel === character.level)
+      if (!characterLimits) {
+        throw new Error('Did not find character limits for character level ' + character.level)
+      }
+      limits.easy += characterLimits.easy
+      limits.medium += characterLimits.medium
+      limits.hard += characterLimits.hard
+      limits.deadly += characterLimits.deadly
+      return limits
+    },
+    { easy: 0, medium: 0, hard: 0, deadly: 0 }
+  )
+  return state
+}
+
 const initializeFromLocalStorage = () => {
   const state = JSON.parse(localStorage.getItem(LOCAL_STORAGE_STATE_KEY))
   return state
@@ -24,22 +42,7 @@ const storeConfig = {
     saveToLocalStorage(state) {
       localStorage.setItem(LOCAL_STORAGE_STATE_KEY, JSON.stringify({ ...state }))
     },
-    updateEncounterLimits(state) {
-      state.encounterLimits = state.characters.reduce(
-        (limits, character) => {
-          const characterLimits = encounterDifficulties.find(ed => ed.characterLevel === character.level)
-          if (!characterLimits) {
-            throw new Error('Did not find character limits for character level ' + character.level)
-          }
-          limits.easy += characterLimits.easy
-          limits.medium += characterLimits.medium
-          limits.hard += characterLimits.hard
-          limits.deadly += characterLimits.deadly
-          return limits
-        },
-        { easy: 0, medium: 0, hard: 0, deadly: 0 }
-      )
-    },
+    updateEncounterLimits,
     setCharacters(state, characters) {
       state.characters = characters
     },
