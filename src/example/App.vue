348e019a85c9bb02c5c636ec3e133657fe522e41
Janne Jaanila <janne.jaanila@gmail.com>
2022-02-20 19:51:28 +0200

    Normalize box-sizing

--- src/example/App.vue
+++ src/example/App.vue
@@ -119,6 +119,17 @@ export default {
 html {
   width: 100%;
   height: 100%;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  -webkit-box-sizing: inherit;
+  -moz-box-sizing: inherit;
+  box-sizing: inherit;
 }
 .page {
   counter-reset: partcounter;
