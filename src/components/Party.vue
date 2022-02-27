51121690c91ee1490927bc664eed2183222acf4f
Janne Jaanila <janne.jaanila@gmail.com>
2022-02-27 17:57:49 +0200

    Add upload state button

--- src/components/Party.vue
+++ src/components/Party.vue
@@ -95,7 +95,18 @@
       </tbody>
     </table>
     <button title="Add party member" @click="addCharacter()">+</button>
-    <button title="Download state" @click.prevent="downloadState()">Download state</button>
+    <button title="Download current Campaigner state" @click.prevent="downloadState()">Download state</button>
+    <button title="Load Campaigner state from disk" @click.prevent="openStateFileSelection()">
+      Upload state
+    </button>
+    <input
+      ref="stateFileInput"
+      accept="application/json"
+      type="file"
+      name="state-file"
+      style="display: none"
+      @change="uploadState"
+    />
     <notebook />
   </div>
 </template>
@@ -127,6 +138,9 @@ export default {
       'addCondition',
       'removeCondition',
     ]),
+    openStateFileSelection() {
+      this.$refs.stateFileInput.click()
+    },
     downloadState() {
       const link = document.createElement('a')
       link.href = URL.createObjectURL(
@@ -137,6 +151,23 @@ export default {
       link.download = `campaigner-state-${new Date().toISOString()}.json`
       link.click()
       URL.revokeObjectURL(link.href)
+    },
+    uploadState(e) {
+      e.target.files[0].text().then(stateText => {
+        try {
+          const state = JSON.parse(stateText)
+          if (confirm('This will overwrite your current Campaigner state. Are you sure?')) {
+            this.$store.replaceState({
+              ...this.$store.state,
+              ...state,
+            })
+          }
+        } catch (err) {
+          console.error(err)
+          alert('Invalid state file')
+          return
+        }
+      })
     },
   },
 }
