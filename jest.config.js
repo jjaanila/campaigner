c1de7b7ecea2ea87b9c2fd676f4ffb0919d28a7b
Janne Jaanila <janne.jaanila@gmail.com>
2022-03-11 18:31:30 +0200

    Run jest only for *.test.js files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   verbose: true,
+  testMatch: ['**/*.test.js'],
   moduleNameMapper: {
     '^project-root/campaign$': '<rootDir>/campaign', // Maps a Webpack alias to correct path. We should probably get rid of the alias
   },
