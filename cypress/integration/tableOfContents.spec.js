c4b4074430c51a5ef92bef6eb2d6d378bdf287a5
Janne Jaanila <janne.jaanila@gmail.com>
2022-03-14 18:21:23 +0200

    Fix ToC tests

--- cypress/integration/tableOfContents.spec.js
+++ cypress/integration/tableOfContents.spec.js
@@ -1,50 +1,55 @@
-it('renders correctly', function () {
-  cy.visit('http://localhost:8080/')
-  cy.get('nav.toc > ol > li').should('have.length', 2)
-  cy.get('nav.toc > ol > li:nth-child(1) > a.toc-part').shouldHaveTrimmedText('Creating a campaign')
-  cy.get('nav.toc > ol > li:nth-child(1) > ol > li').should('have.length', 2)
-  cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > a.toc-chapter').shouldHaveTrimmedText(
-    'Getting started'
-  )
-  cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li').should('have.length', 3)
-  cy.get(
-    'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li:nth-child(1) > a.toc-section'
-  ).shouldHaveTrimmedText('Installing Campaigner')
-  cy.get(
-    'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li:nth-child(2) > a.toc-section'
-  ).shouldHaveTrimmedText('Campaign building blocks')
-  cy.get(
-    'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li:nth-child(3) > a.toc-section'
-  ).shouldHaveTrimmedText('Running Campaigner')
-  cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > a.toc-chapter').shouldHaveTrimmedText(
-    'Components'
-  )
-  cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li').should('have.length', 3)
-  cy.get(
-    'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li:nth-child(1) > a.toc-section'
-  ).shouldHaveTrimmedText('read-aloud')
-  cy.get(
-    'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li:nth-child(2) > a.toc-section'
-  ).shouldHaveTrimmedText('encounter')
-  cy.get(
-    'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li:nth-child(3) > a.toc-section'
-  ).shouldHaveTrimmedText('table-of-contents')
-  cy.get('nav.toc > ol > li:nth-child(2) > a.toc-part').shouldHaveTrimmedText('Resources')
-})
+describe('TableOfContents', () => {
+  it('renders correctly', function () {
+    cy.visit('http://localhost:8080/')
+    cy.get('nav.toc > ol > li').should('have.length', 2)
+    cy.get('nav.toc > ol > li:nth-child(1) > a.toc-part').shouldHaveTrimmedText('Creating a campaign')
+    cy.get('nav.toc > ol > li:nth-child(1) > ol > li').should('have.length', 2)
+    cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > a.toc-chapter').shouldHaveTrimmedText(
+      'Getting started'
+    )
+    cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li').should('have.length', 3)
+    cy.get(
+      'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li:nth-child(1) > a.toc-section'
+    ).shouldHaveTrimmedText('Installing Campaigner')
+    cy.get(
+      'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li:nth-child(2) > a.toc-section'
+    ).shouldHaveTrimmedText('Campaign building blocks')
+    cy.get(
+      'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(1) > ol > li:nth-child(3) > a.toc-section'
+    ).shouldHaveTrimmedText('Running Campaigner')
+    cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > a.toc-chapter').shouldHaveTrimmedText(
+      'Components'
+    )
+    cy.get('nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li').should('have.length', 4)
+    cy.get(
+      'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li:nth-child(1) > a.toc-section'
+    ).shouldHaveTrimmedText('read-aloud')
+    cy.get(
+      'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li:nth-child(2) > a.toc-section'
+    ).shouldHaveTrimmedText('encounter')
+    cy.get(
+      'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li:nth-child(3) > a.toc-section'
+    ).shouldHaveTrimmedText('table-of-contents')
+    cy.get(
+      'nav.toc > ol > li:nth-child(1) > ol > li:nth-child(2) > ol > li:nth-child(4) > a.toc-section'
+    ).shouldHaveTrimmedText('captioned-figure')
+    cy.get('nav.toc > ol > li:nth-child(2) > a.toc-part').shouldHaveTrimmedText('Resources')
+  })
 
-it('is functional', function () {
-  cy.visit('http://localhost:8080/')
-  cy.get('.page').click({ force: true })
-  cy.get('#show-toc-always', { timeout: 10000 }).should('be.visible')
-  cy.get('#show-toc-always').uncheck()
-  cy.get('.page').click({ force: true })
-  cy.get('#show-toc-always', { timeout: 10000 }).should('not.exist')
-  cy.get('nav.toc').click()
-  cy.get('#show-toc-always', { timeout: 10000 }).should('be.visible')
-  cy.get('#show-toc-always').check()
-  cy.get('.toc .toc-part,.toc-chapter,.toc-section').should('have.attr', 'href')
-  cy.get('.toc .toc-part,.toc-chapter,.toc-section').each($a => {
-    cy.wrap($a).click()
-    cy.get($a.attr('href')).should('be.visible')
+  it('is functional', function () {
+    cy.visit('http://localhost:8080/')
+    cy.get('.page').click({ force: true })
+    cy.get('#show-toc-always', { timeout: 10000 }).should('be.visible')
+    cy.get('#show-toc-always').uncheck()
+    cy.get('.page').click({ force: true })
+    cy.get('#show-toc-always', { timeout: 10000 }).should('not.exist')
+    cy.get('nav.toc').click()
+    cy.get('#show-toc-always', { timeout: 10000 }).should('be.visible')
+    cy.get('#show-toc-always').check()
+    cy.get('.toc .toc-part,.toc-chapter,.toc-section').should('have.attr', 'href')
+    cy.get('.toc .toc-part,.toc-chapter,.toc-section').each($a => {
+      cy.wrap($a).click()
+      cy.get($a.attr('href')).should('be.visible')
+    })
   })
 })
